7f54bc131ff2889e7974b7cd7d8443eeb62350c6
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-12-11 19:37:07 +0900

    feat: layoutの読み込み

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ const templatePath = "./templates"
 const layoutPath = templatePath + "/layout.html"
 
 var (
-	templates = template.Must(template.ParseGlob(templatePath + "/index.html"))
+	templates = template.Must(template.ParseFiles(layoutPath, templatePath+"/index.html"))
 )
 
 func main() {
@@ -19,8 +19,8 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", map[string]interface{}{
-		"Title": "Hello World",
-		"Text":  "Hello World",
+	templates.ExecuteTemplate(w, "layout.html", map[string]interface{}{
+		"PageTitle": "Home Page",
+		"Text":      "Hello World",
 	})
 }
